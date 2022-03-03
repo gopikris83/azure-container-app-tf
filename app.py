88ed07e3fc88aa5e6abd205bb7b65e7fdaeecde8
Gopi Krishna <gopikrishna.83@gmail.com>
2022-03-03 15:14:52 +0100

    Code changed

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def welcome_message():
     info = "Welcome to 2022!"
-    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
-    r = requests.get("https://httpbin.org/headers", headers=user_agent)
-    return render_template('index.html', welcome_message=info, user_agent_info=r.text)
+    r = requests.utils.default_headers()
+    return render_template('index.html', welcome_message=info, user_agent_info=r['User-Agent'])
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
